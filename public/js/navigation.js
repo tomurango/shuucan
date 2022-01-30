df119da6fd184724364bc1945295844e3a2e5fdb
Eiji TOMURA <s1f101803758@iniad.org>
2022-01-30 15:03:33 +0900

    20221030

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,13 +1,18 @@
 function tag_danjon(element){
-    if(element.id == "danjon_look"){
-        element.classList.toggle("active");
-        element.classList.toggle("disactive");
-        document.getElementById("danjon_follow").classList.toggle("active");
-        document.getElementById("danjon_follow").classList.toggle("disactive");
-    }else if(element.id == "danjon_follow"){
-        element.classList.toggle("active");
-        element.classList.toggle("disactive");
-        document.getElementById("danjon_look").classList.toggle("active");
-        document.getElementById("danjon_look").classList.toggle("disactive");
+    if(element.classList.contains("disactive")){
+        if(element.id == "danjon_look"){
+            element.classList.toggle("active");
+            element.classList.toggle("disactive");
+            document.getElementById("danjon_follow").classList.toggle("active");
+            document.getElementById("danjon_follow").classList.toggle("disactive");
+        }else if(element.id == "danjon_follow"){
+            element.classList.toggle("active");
+            element.classList.toggle("disactive");
+            document.getElementById("danjon_look").classList.toggle("active");
+            document.getElementById("danjon_look").classList.toggle("disactive");
+        }
+    }else{
+        //activeのボタンを押したので何もしないで関数を終了させる
+        return ;
     }
 }
