ef13d20749002b8f287284af6b7334cbaa8708ba
Eiji TOMURA <s1f101803758@iniad.org>
2022-02-02 00:08:05 +0900

    20220201

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -20,3 +20,26 @@ function tag_danjon(element){
         return ;
     }
 }
+
+function tag_world(element){
+    if(element.classList.contains("disactive")){
+        if(element.id == "world_look"){
+            element.classList.toggle("active");
+            element.classList.toggle("disactive");
+            document.getElementById("world_follow").classList.toggle("active");
+            document.getElementById("world_follow").classList.toggle("disactive");
+            //document.getElementById("danjon_look_page").style.display = "block";
+            //document.getElementById("danjon_follow_page").style.display = "none";
+        }else if(element.id == "world_follow"){
+            element.classList.toggle("active");
+            element.classList.toggle("disactive");
+            document.getElementById("world_look").classList.toggle("active");
+            document.getElementById("world_look").classList.toggle("disactive");
+            //document.getElementById("danjon_follow_page").style.display = "block";
+            //document.getElementById("danjon_look_page").style.display = "none";
+        }
+    }else{
+        //activeのボタンを押したので何もしないで関数を終了させる
+        return ;
+    }
+}
