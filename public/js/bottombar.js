1fb3fdff259d648c817ffef41a8a924beec2fd34
Eiji TOMURA <s1f101803758@iniad.org>
2022-04-11 06:40:01 +0900

    20220411

--- public/js/bottombar.js
+++ public/js/bottombar.js
@@ -13,33 +13,35 @@ function page_nav(index){
         top_level_pages[i].style.display = "none";
     }
     //sidenavボタンをすべてdisactive
-    document.getElementById("side_world").classList.remove("active");
+    //document.getElementById("side_world").classList.remove("active");
     document.getElementById("side_danjon").classList.remove("active");
     document.getElementById("side_premia").classList.remove("active");
     document.getElementById("side_set").classList.remove("active");
     //indexによって処理を分岐して記述する
-    if(index==0){
+    /*if(index==0){
+        // world 消したからとりあえずうごかん
         //ワールドページ表示
         document.getElementById("world_page").style.display = "flex";
         //sideのactive
         document.getElementById("side_world").classList.add("active");
         //headerの名前表示切替
         document.getElementById("name_on_header").textContent="ワールド";
-    }else if(index==1){
+    }else */
+    if(index==0){
         //ダンジョンページ表示
         document.getElementById("danjon_page").style.display = "flex";
         //sideのactive
         document.getElementById("side_danjon").classList.add("active");
         //headerの名前表示切替
         document.getElementById("name_on_header").textContent="ダンジョン";
-    }else if(index==2){
+    }else if(index==1){
         //プレミアページ表示
         document.getElementById("premia_page").style.display = "flex";
         //sideのactive
         document.getElementById("side_premia").classList.add("active");
         //headerの名前表示切替
         document.getElementById("name_on_header").textContent="プレミア";
-    }else if(index==3){
+    }else if(index==2){
         //セッテイページ表示
         document.getElementById("set_page").style.display = "flex";
         //sideのactive
