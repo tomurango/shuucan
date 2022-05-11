bc27e64b7f22333bf2ad954efbabe366960da5e6
Eiji TOMURA <s1f101803758@iniad.org>
2022-05-12 07:39:40 +0900

    20220512

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -8,7 +8,7 @@ function tag_danjon(element){
             document.getElementById("danjon_look_page").style.display = "flex";
             document.getElementById("danjon_follow_page").style.display = "none";
             //dungeonの情報を取ってくる見た目を整えて挿入準備が完了したらコメントアウトを解除して運用開始
-            /*dungeon_look();*/
+            dungeon_look();
         }else if(element.id == "danjon_follow"){
             element.classList.toggle("active");
             element.classList.toggle("disactive");
@@ -57,8 +57,9 @@ function dungeon_look(){
             //loop
             console.log("dungeons 数", dungeons.size);
             dungeons.forEach(function(dungeon){
-                //完了タスクとそうじゃないタスクに振り分ける
-                console.log(dungeon.data(), dungeon.id);
+                //ダンジョンごとに挿入していく
+                //console.log(dungeon, dungeon.data(), dungeon.id);
+                insert_dungeon(dungeon.data(), dungeon.id);
             });
         }).catch(function(error){
             console.log("error", error);
@@ -83,14 +84,73 @@ function dungeon_detail_back(){
 
 function dungeon_follow(ele){
     //すべてのfoldan_btnをdisactiveにする
-
+    $('.foldan_btn').removeClass('active');
     var ele_id = ele.id;
     console.log(ele_id);
+    var query_id = '#' + ele_id;
     //選択したfoldan_btnをactiveにする
-
+    $(query_id).addClass('active');
     if(ele_id == "fol_dan_all"){
         console.log("display all followed !");
     }else{
         console.log("display choised");
     }
+    //まずはデータベースの確認（global変数を含めて）をした後に入れ込んでく形で行こうか
+    //その前にアプリ読み込み時にfollow dungeon allの情報を取得してから挿入するまでの流れを作る
+    //その前にダンジョンに取り組んだ人の挑戦を記録できるようにする
+    //その前に今あるダンジョンを取得してきてちゃんと表示できるようにする
+    //ダンジョンの作成はとりあえず私側で手動にて行うので、実装ん面は取得からの挿入で問題ないと考える
+
+    //#fol_dan_thread のなかにしていされた dungeon の情報を入れ込んでいく
+    //document.getElementById()
+
 }
+
+//だんじょんを挿入する関数
+function insert_dungeon(dnj_data, dnj_id){
+    //console.log(dnj_data, dnj_id);
+    //つづっととかの挿入関数もってくる
+    /*
+    <div id="" class="mdc-card danjon_card" onclick="dungeon_detail(this)">
+                <div id="" class="mdc-card__media mdc-card__media--16-9 dnj_media_thum">
+                </div>
+                <div class="mdc-card__content team_info_container">
+                  <p class="danjon_name">ダンジョン名</p>
+                  <p class="danjon_first">１ 目標を小さくする</p>
+                </div>
+              </div>
+              */
+
+    
+
+    var dnj_card = '<div id="' + dnj_id + '_dnjcard" class="mdc-card danjon_card" onclick="dungeon_detail(this)"><div id="" class="mdc-card__media mdc-card__media--16-9 dnj_media_thum"></div><div class="mdc-card__content team_info_container"><p class="danjon_name"></p><p class="danjon_first"></p></div></div>';
+
+    
+    var cards_container = document.getElementById("dnj-card-container");
+    var dnj_card_promise = new Promise(function(resolve, reject){
+        cards_container.insertAdjacentHTML("afterbegin", dnj_card);
+        resolve();
+    });
+    var queryid = "#" + dnj_id + "_dnjcard"; 
+    dnj_card_promise.then(function(){
+        //つづきはここから
+        //ここでtextContentいれる
+        $(queryid).find(".danjon_name").text(dnj_data.name);
+        $(queryid).find(".danjon_first").text(dnj_data.explain);
+        $(queryid).find(".dnj_media_thum").css({
+            backgroundImage: 'url("'+ dnj_data.imageURL +'")' // "" で括っていないとIEでは表示されない
+        });
+        /*
+        //過去にボタンを押したかどうかで処理を追加で書いていこう
+        if(data.goodList.includes(global_user.uid)){
+            //goodListにある
+            $(queryid).find(".good").addClass("active");
+        }else if(data.badList.includes(global_user.uid)){
+            //badList
+            $(queryid).find(".bad").addClass("active");
+        }
+        */
+    });
+}
+
+
